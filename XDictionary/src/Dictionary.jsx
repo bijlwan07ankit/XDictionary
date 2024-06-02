2dbb13e7d8cbb36f8bb07393ebfb0d26e459edb5
bijlwan07ankit <bijlwan07ankit@gmail.com>
2024-06-02 10:03:17 +0530

    xdictionary done

--- XDictionary/src/Dictionary.jsx
+++ XDictionary/src/Dictionary.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import { useState } from 'react';
 function Dictionary() {
-    const [word, setWord]=useState("");
-    function handleSearchbtn(e){
-   let text = e.target.value;
-   setWord(text);
+    const dictionary=[
+        {
+            word: "React",
+            meaning: "A JavaScript library for building user interfaces.",
+          },
+      
+          { word: "Component", meaning: "A reusable building block in React." },
+      
+          { word: "State", meaning: "An object that stores data for a component." },
+        ];
+      
+    const [text, setText] = useState("");
+    const [show, setShow] = useState("");
+    let [ans, setAns] = useState(null);
+
+    function handleInput(e){
+        setShow("")
+   let searchText = e.target.value;
+   setText(searchText);
     }
+    const handleShow = () => {
+        for (let i = 0; i < dictionary.length; i++) {
+          if (dictionary[i].word.toLowerCase() === text.toLowerCase()) {
+            setShow(dictionary[i].meaning);
+            setAns(true);
+            return;
+          } else {
+            setAns(false);
+          }
+        }
+    }
+
   return (
    <>
    <h1>Dictionary App </h1>
@@ -13,9 +40,12 @@ function Dictionary() {
    type="text"
 //    value={word}
    placeholder="Search for a Word.."
+   onChange={(e)=> (handleInput(e))}
    />
-   <button onClick={handleSearchbtn}>Search</button>
+   <button onClick={handleShow}>Search</button>
    <h5>Definition:</h5>
+   {ans && <p>{show}</p>}
+      {ans === false && <p>Word not found in the dictionary.</p>}
    </>
   )
 }
